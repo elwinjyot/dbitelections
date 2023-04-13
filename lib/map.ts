e8cd14f843d7a74ab96cd807c29063b5e059ae47
elwinjyot <elwinjyothisr444@gmail.com>
2023-04-13 14:21:12 +0530

    Final push

---
+++ lib/map.ts
@@ -0,0 +1,21 @@
+// interface Map {
+//     index: number;
+//     ids: string[];
+// }
+
+const pageMap: { [key: number]: string[] } = {
+    1: ["clgdj2mov000gtrt492wxqon1", "clgeqev2d0000vv2wtgk2x3e3"],
+    2: ["clgdj4izc000itrt4z9rj6fmz", "clgeqgiqe0002vv2wgfe2zsbj"],
+    3: ["clgdj4zzm000ktrt4kj36zdag", "clgeqhc4p0004vv2wq1yafv36"],
+    4: ["clgdj5mqd000mtrt4bltnld53", "clgeqimdl0006vv2whl2uk0a2"],
+    5: ["clgdj6lic000otrt4y1wwsbcs", "clgeqz3cy0000vvakx3wgmcuc"],
+    6: ["clgdj7nne000qtrt4d6w6541o", "clgeqzhsd0002vvakwfd0kw3w"],
+    7: ["clgdj8jhr000strt4z6lvpqid", "clger0cod0004vvak6dsm2abg"],
+    8: ["clgdj9v4e000wtrt4wzw4dqpz", "clger1w510006vvaki56sdntx"],
+    9: ["clgdjajha0010trt49by5jf2b", "clgerslsb0006vvt81odavvmq"],
+    10: ["clgdjbd1z0012trt4fwx512hx", "clgerlmkw0000vvt8nvj7uqzi"],
+    11: ["clgdjcmtx0014trt4i37llrq9", "clgermsgs0002vvt8mkcd1gkz"],
+    12: ["clgerx56i0008vvt8yhh7q3k3", "clgerztpu000avvt8af51wr9y", "clges1bea000cvvt83kixbl0r", "clges1ogx000evvt8kvi5lu0q", "clgesbk01000gvvt8a96cmmt3", "clgesbslv000ivvt8tusi0389", "clgesc8yq000kvvt8716n6fnr", "clgesdyab000mvvt8ledi4jgb"],
+}
+
+export default pageMap;
