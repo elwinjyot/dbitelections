9e3a087705d354fb29802fd1c9b19e7b033d15c0
elwinjyot <elwinjyothisr444@gmail.com>
2023-04-16 22:05:55 +0530

    Front image changed

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ["res.cloudinary.com","imgs.search.brave.com"]
+    domains: ["res.cloudinary.com","imgs.search.brave.com", "ezgsausrazdmuxcqepvl.supabase.co"]
   }
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import logo from "../assets/images/dbitlogo.png";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <section id="home" className="bg-primary container-full">
-      <Image src={logo} alt="" />
+      <Image src={"https://ezgsausrazdmuxcqepvl.supabase.co/storage/v1/object/public/images/WhatsApp%20Image%202023-04-13%20at%2016.02.59.jpg?t=2023-04-16T16%3A34%3A01.015Z"} width={400} height={400} alt="" />
       <div className="landing-text">
         <h1>Don Bosco Institute of Technology</h1>
         <p>Online Elections Portal</p>
